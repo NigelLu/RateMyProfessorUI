8d0d3d8e7fb380cc4700e42b170e081fca95d167
NigelLu <nigellu@outlook.com>
2023-11-11 16:21:39 -0500

    init: add boilertemplate files and pre-commit hooks

---
+++ src/App.js
@@ -0,0 +1,17 @@
+/** @format */
+
+import "./App.scss";
+
+function App() {
+  return (
+    <div className='App'>
+      <header className='App-header'>
+        <p>
+          Welcome to <b>Rate My Professor</b>!
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
